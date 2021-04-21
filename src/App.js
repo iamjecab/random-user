0d62c7ef999fafe6b0c09dc8d8a9b51342e17c37
vicky <vicky.7dev@gmail.com>
2021-04-21 11:03:44 +0530

    initial commit setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GlobalStyles = createGlobalStyle`
+  *{
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+  }
+`;
+
+const App = () => {
+    return (
+        <>
+            {" "}
+            <GlobalStyles />
+            <h1>hello</h1>
+        </>
+    );
+};
 
 export default App;
